403a51cd7f9f979d63d5c2aee3436d9240e5bfc2
SupriyaAdlapur-5 <adlapursupriya5@gmail.com>
2024-11-18 16:28:27 +0530

    String Reduce.py

--- R P S.py
+++
@@ -1,8 +0,0 @@
-    vignesh, charan = input().split()
-    if(vignesh == 'R' and charan == 'S') or (vignesh == 'P' and charan == 'R') or (vignesh == 's' and charan == 'P'):
-        print("vignesh")
-    elif (charan == 'R' and  vignesh == 'S') or (charan == 'P' and vignesh == 'R') or (charan == 'S' and vignesh == 'P'):
-        print("charan")
-    else:
-        print("NULL")
-    
